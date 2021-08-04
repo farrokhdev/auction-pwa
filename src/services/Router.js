51bc3a7ab369262dfb282f54638873fe3e694cd2
amnmoj-117 <md.ramezanpoor@gmail.com>
2021-08-04 09:37:28 +0430

    change not found page

--- src/services/Router.js
+++ src/services/Router.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+import {Switch, Route, BrowserRouter as Router} from 'react-router-dom'
 import NotFound from '../components/not-found'
 import LoginPage from '../Views/Auth/Login'
-import Footer from '../components/footer'
 import Account from '../Views/Account/Account'
 import AccountChangepassword from '../Views/Account/AccountChangePassword/AccountChangepassword'
 import AccountMessages from '../Views/Account/AccountMessage/AccountMessages'
@@ -14,21 +13,24 @@ const RouterConfig = () => {
         <Router>
             <Switch>
                 <Route path="/auth"
-                    render={({ match: { url } }) => (
-                        <Route path={`${url}/login`} component={() => <LoginPage />} />
-                    )} />
+                       render={({match: {url}}) => (
+                           <Route path={`${url}/login`} component={() => <LoginPage/>}/>
+                       )}/>
 
                 <Route path="/account"
-                    render={({ match: { url } }) => (
-                        <>
-                            <Route exact path={`${url}/`} component={() => <Account />} />
-                            <Route exact path={`${url}/change-password`} component={() => <AccountChangepassword />} />
-                            <Route exact path={`${url}/messages`} component={() => <AccountMessages />} />
-                            <Route exact path={`${url}/ticket-detail`} component={() => <AccountMessagesTicketDetail />} />
-                            <Route exact path={`${url}/my-auctions`} component={() => <AccountMyAuctions />} />
-                        </>
-                    )} />
+                       render={({match: {url}}) => (
+                           <>
+                               <Route exact path={`${url}/`} component={() => <Account/>}/>
+                               <Route exact path={`${url}/change-password`} component={() => <AccountChangepassword/>}/>
+                               <Route exact path={`${url}/messages`} component={() => <AccountMessages/>}/>
+                               <Route exact path={`${url}/ticket-detail`}
+                                      component={() => <AccountMessagesTicketDetail/>}/>
+                               <Route exact path={`${url}/my-auctions`} component={() => <AccountMyAuctions/>}/>
+                           </>
+                       )}/>
+                <Route path={`*`} component={() => <NotFound/>}/>
             </Switch>
+
         </Router>
     )
 }
