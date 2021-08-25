1ffae52cbbf6e99648b6fefc511c9982d339b079
ehsan mashali <ehsanmashali222.78@gmail.com>
2021-08-25 18:17:08 +0430

    fixed a bug

--- src/Views/AuctionRegistration/BuyerRegister.js
+++ src/Views/AuctionRegistration/BuyerRegister.js
@@ -1,52 +1,52 @@
-import React, {useEffect, useState} from "react";
-import Footer from '../../components/footer';
-import axios from "../../utils/request";
-import {BASE_URL} from "../../utils";
-import {Redirect} from "react-router-dom";
-import AuctionRegistration from "./AuctionRegistration";
+// import React, {useEffect, useState} from "react";
+// import Footer from '../../components/footer';
+// import axios from "../../utils/request";
+// import {BASE_URL} from "../../utils";
+// import {Redirect} from "react-router-dom";
+// import AuctionRegistration from "./AuctionRegistration";
 
-function BuyerRegister(props) {
-    const [hasPerm, setHasPerm] = useState(true);
-    const [redirectUrl, setRedirectUrl] = useState("");
+// function BuyerRegister(props) {
+//     const [hasPerm, setHasPerm] = useState(true);
+//     const [redirectUrl, setRedirectUrl] = useState("");
 
-    const getProfile = () => {
-        axios.get(`${BASE_URL}/account/profile/`)
-            .then(resp => {
-                if (resp.data.code === 200) {
-                    if (!resp.data.data.result.complete_profile) {
-                        setRedirectUrl("/account/my-profile")
-                        setHasPerm(false)
-                    }
-                    else if (!resp.data.data.result.complete_bank_info) {
-                        setRedirectUrl("/auction-registration/financialinfo")
-                        setHasPerm(false) 
-                    }
-                    else {
-                        setHasPerm(true)
-                    }
-                }
-            })
-            .catch(err => {
-                console.error(err);
-            })
-    }
+//     const getProfile = () => {
+//         axios.get(`${BASE_URL}/account/profile/`)
+//             .then(resp => {
+//                 if (resp.data.code === 200) {
+//                     if (!resp.data.data.result.complete_profile) {
+//                         setRedirectUrl("/account/my-profile")
+//                         setHasPerm(false)
+//                     }
+//                     else if (!resp.data.data.result.complete_bank_info) {
+//                         setRedirectUrl("/auction-registration/financialinfo")
+//                         setHasPerm(false) 
+//                     }
+//                     else {
+//                         setHasPerm(true)
+//                     }
+//                 }
+//             })
+//             .catch(err => {
+//                 console.error(err);
+//             })
+//     }
 
-    useEffect(() => {
-        getProfile()
-    }, [])
+//     useEffect(() => {
+//         getProfile()
+//     }, [])
 
-    if (hasPerm) {
-        return (
-            <>
-                <AuctionRegistration id={props.match.params.id}/>
-                <Footer/>
-            </>
-        )
-    } else {
-        return (
-            <Redirect to={redirectUrl}/>
-        )
-    }
-}
+//     if (hasPerm) {
+//         return (
+//             <>
+//                 <AuctionRegistration id={props.match.params.id}/>
+//                 <Footer/>
+//             </>
+//         )
+//     } else {
+//         return (
+//             <Redirect to={redirectUrl}/>
+//         )
+//     }
+// }
 
-export default BuyerRegister;
+// export default BuyerRegister;
